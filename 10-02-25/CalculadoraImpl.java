4e71e43f234df5ee0973a25057430e6b4a2f47de
David <david_rojasavalos@hotmail.com>
2025-02-10 19:41:29 -0600

    calculadora

--- 10-02-25/CalculadoraImpl.java
+++ 10-02-25/CalculadoraImpl.java
@@ -4,6 +4,12 @@ class CalculadoraImpl implements ICalculadora {
 		switch (operacion.getOperando()) {
 			case SUMA:
 				return operacion.getx1() + operacion.getx2();
+			case RESTA:
+				return operacion.getx1() - operacion.getx2();
+			case DIV:
+				return operacion.getx1() / operacion.getx2();
+			case MULT:
+				return operacion.getx1() * operacion.getx2();
 			default:
 				return 0.0;
 		}
--- 10-02-25/Main.java
+++ 10-02-25/Main.java
@@ -1,9 +1,27 @@
 import java.io.*;
+import java.util.Scanner;
+
 class Main{
 	public static void main(String [] args){
-		Operacion op = new Operacion(10.0, 10.0, Operando.SUMA);
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Ingresa el primer numero: ");
+		double num1 = scanner.nextDouble();
+
+		System.out.println("Ingrese el segundo numero: ");
+		double num2 = scanner.nextDouble();
+		
+		System.out.println("Que operacion deseas realizar(SUMA, RESTA, DIV, MULT): ");
+		String operacionStr = scanner.next().toUpperCase();
+
+		Operando operacion = Operando.valueOf(operacionStr);
+
+		Operacion op = new Operacion(num1, num2, operacion);
 		ICalculadora calculadora = new CalculadoraImpl();
 		Double resultado = calculadora.calcular(op);
 		System.out.println("Resultado:" + resultado);
+
+		scanner.close();
 	}
 }
